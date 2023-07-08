564c489d8aa4907cf38e77e55201aa4e9566c050
mbirali <mohamed.birali@usms.ac.ma>
2023-07-08 15:00:49 +0100

    Setup ts env

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { log } from "console";
+
+const language: string = "Typescript"
+
+function hello() {
+    log(`\n \n`);
+    log(`******************************`);
+    log(`* I'm learning ${language} ****`);
+    log(`******************************`);
+    log(`\n \n`);
+}
+
+hello();
